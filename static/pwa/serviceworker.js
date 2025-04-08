f92b29860fd8727ccee753825a344a68b2bca832
Alghard <julien.varela@gmail.com>
2025-04-08 18:55:29 +0200

    pwa config and urls #24

---
+++ static/pwa/serviceworker.js
@@ -0,0 +1,44 @@
+const staticCacheName = 're-sources-cache-v1';
+
+const filesToCache = [
+    '/',
+    '/static/css/output.css',
+    '/static/pwa/offline.html'
+];
+
+// Installation du service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(staticCacheName)
+        .then(cache => {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+// Activation du service worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(cacheName => cacheName.startsWith('re-sources-'))
+                    .filter(cacheName => cacheName !== staticCacheName)
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request)
+                    .catch(() => {
+                        return caches.match('/static/pwa/offline.html');
+                    });
+            })
+    );
+});
