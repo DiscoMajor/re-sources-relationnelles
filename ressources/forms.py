99ab3b844ede880ea47853f0e954efbc13c38743
Victor <62471302+DiscoMajor@users.noreply.github.com>
2025-04-09 19:01:45 +0200

    Template updates #47

--- ressources/forms.py
+++ ressources/forms.py
@@ -6,8 +6,16 @@ class RessourceForm(forms.ModelForm):
         model = Ressource
         fields = ['title', 'type', 'is_private']
         widgets = {
-            'is_private': forms.CheckboxInput(),
+            'title': forms.TextInput(attrs={
+                'class': 'block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+            }),
+            'type': forms.Select(attrs={
+                'class': 'block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+            }),
+            'is_private': forms.CheckboxInput(attrs={
+                'class': 'h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'
+            }),
         }
         labels = {
-                'is_private': 'Rendre cette ressource privée (visible uniquement par vos amis)',
-            }
+            'is_private': 'Rendre cette ressource privée (visible uniquement par vos amis)',
+        }
