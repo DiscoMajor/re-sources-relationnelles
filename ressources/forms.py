e31c7da90c5ac72dd89844e4b8aac388887436fc
Victor <62471302+DiscoMajor@users.noreply.github.com>
2025-04-11 13:44:28 +0200

    Templates update with Tailwind #64

--- ressources/forms.py
+++ ressources/forms.py
@@ -7,13 +7,13 @@ class RessourceForm(forms.ModelForm):
         fields = ['title', 'type', 'is_private']
         widgets = {
             'title': forms.TextInput(attrs={
-                'class': 'block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                'class': 'block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm'
             }),
             'type': forms.Select(attrs={
-                'class': 'block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                'class': 'block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm'
             }),
             'is_private': forms.CheckboxInput(attrs={
-                'class': 'h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'
+                'class': 'h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded'
             }),
         }
         labels = {
